231397a3c11074fd8598e2a6f4618f17ce5fedc9
mwunnam <mwunnam@gmail.com>
2025-08-14 23:14:29 +0000

    corrections

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,5 +7,9 @@ from .views import (
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='books-list-create'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='detail-update-destory'),
+    path('books/create/',),
+    path('books/update/',),
+    path('books/<int:pk>/',),
+    path('books/<int:pk>/', ),
 ]
 
